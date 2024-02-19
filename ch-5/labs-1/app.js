f970a45ce4539278660423289239e99b4032e5ed
lontsimesmer <tchoupounathanael@gmail.com>
2024-02-19 23:54:14 +0100

    resolve prefixer exercise

--- ch-5/labs-1/app.js
+++ ch-5/labs-1/app.js
@@ -1,9 +1,16 @@
 "use strict";
-function prefixerFunctx(prefix) {
-  const sayHiTo = prefixer("Hello");
-  const sayByeTo = prefixer("Goodbye");
-  console.log(sayHiTo("Dave")); // prints 'Hello Dave'
-  console.log(sayHiTo("Annie")); // prints 'Hello Annie'
-  console.log(sayByeTo("Dave")); // prints 'Goodbye Dave'
+function prefixer(name) {
+  return function (message) {
+    if (message.includes("Hello")) {
+      return "Hello" + name;
+    } else {
+      return "Goodbye" + name;
+    }
+  };
 }
-prefixFunction();
+
+const sayHiTo = prefixer("Hello");
+const sayByeTo = prefixer("Goodbye");
+console.log(sayHiTo("Dave")); // prints 'Hello Dave'
+console.log(sayHiTo("Annie")); // prints 'Hello Annie'
+console.log(sayByeTo("Dave")); // prints 'Goodbye Dave'

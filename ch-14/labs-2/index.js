303f553d46ba7990884845a2fa2c0e6696a596bc
lontsimesmer <tchoupounathanael@gmail.com>
2024-03-17 23:55:36 +0100

    add os uptime and memory

--- ch-14/labs-2/index.js
+++ ch-14/labs-2/index.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const os = require("os");
+
+const inSecondsUptime = os.uptime();
 const uptimeInSeconds = process.uptime();
+const totalMemoryInBytes = os.totalmem();
+const totalMemoryInGB = Math.floor(totalMemoryInBytes / (1024 * 1024 * 1024));
 
 setTimeout(() => {
   console.log("Process uptime:", uptimeInSeconds, "seconds"); // TODO output uptime of process
-  console.log(); // TODO output uptime of OS
-  console.log(); // TODO output total system memory
+  console.log("Operating System uptime:", inSecondsUptime, "seconds"); // TODO output uptime of OS
+  console.log("Total System Memory:", totalMemoryInGB, "GB"); // TODO output total system memory
   console.log(); // TODO output total heap memory
 }, 1000);

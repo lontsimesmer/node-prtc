798f623b3747f3df8bb1ffb3a09aadd5a401e9cc
lontsimesmer <tchoupounathanael@gmail.com>
2024-03-18 23:47:00 +0100

    add os uptime and memory

--- ch-14/labs-2/index.js
+++ ch-14/labs-2/index.js
@@ -3,18 +3,18 @@
 const os = require("os");
 const v8 = require("v8");
 
-const inSecondsUptime = os.uptime();
-const uptimeInSeconds = process.uptime();
+// const inSecondsUptime = os.uptime();
+// const uptimeInSeconds = process.uptime();
 
-const totalMemoryInBytes = os.totalmem();
-const totalMemoryInGB = Math.floor(totalMemoryInBytes / (1024 * 1024 * 1024));
+// const totalMemoryInBytes = os.totalmem();
+// const totalMemoryInGB = Math.floor(totalMemoryInBytes / (1024 * 1024 * 1024));
 
-const totalHeapSizeInBytes = v8.getHeapStatistics().total_heap_size;
-const totalHeapSizeInMB = Math.floor(totalHeapSizeInBytes / (1024 * 1024));
+// const totalHeapSizeInBytes = v8.getHeapStatistics().total_heap_size;
+// const totalHeapSizeInMB = Math.floor(totalHeapSizeInBytes / (1024 * 1024));
 
 setTimeout(() => {
-  console.log("Process uptime:", uptimeInSeconds, "seconds"); // TODO output uptime of process
-  console.log("Operating System uptime:", inSecondsUptime, "seconds"); // TODO output uptime of OS
-  console.log("Total System Memory:", totalMemoryInGB, "GB"); // TODO output total system memory
-  console.log("Total Heap Memory", totalHeapSizeInMB, "MB"); // TODO output total heap memory
+  console.log(process.uptime()); // TODO output uptime of process
+  console.log(os.uptime()); // TODO output uptime of OS
+  console.log(os.totalmem()); // TODO output total system memory
+  console.log(v8.getHeapStatistics().total_heap_size); // TODO output total heap memory
 }, 1000);

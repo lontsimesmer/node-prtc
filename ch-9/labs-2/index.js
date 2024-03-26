30910adf7e8c8beda325d53e6f61001e96993892
lontsimesmer <tchoupounathanael@gmail.com>
2024-03-26 23:30:59 +0100

    correct timeout error

--- ch-9/labs-2/index.js
+++ ch-9/labs-2/index.js
@@ -1,9 +1,13 @@
 "use strict";
 const { EventEmitter } = require("events");
 
+process.on("uncaughtException", (err) => {
+  console.log(err.message);
+});
+
 process.nextTick(console.log, "passed!");
 
 const ee = new EventEmitter();
 
 ee.once("error", (err) => console.log(err.message));
-ee.emit("error", Error("timeout"));
+// ee.emit("error", Error("timeout"));

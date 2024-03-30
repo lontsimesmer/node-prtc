8bd7ec1d15c08c3f9fdc4ad97d2b1dc591e54140
lontsimesmer <tchoupounathanael@gmail.com>
2024-03-30 15:36:34 +0100

    add prototypal inheritance

--- ch-5/labs-2/app.js
+++ ch-5/labs-2/app.js
@@ -1,26 +1,43 @@
-const assert = require('assert')
+const assert = require("assert");
 
-// TODO: 
-// implement a way to create a prototype chain
-// of leopard -> lynx -> cat
-// leopard prototype must have ONLY a hiss method
-// lynx prototype must have ONLY a purr method
-// cat prototype must have ONLY a meow method
+const leopard = {
+  hiss() {
+    console.log("Felix the cat: hsss");
+  },
+};
 
-const felix = null //TODO replace null with instantiation of a cat
-felix.meow() // prints Felix the cat: meow
-felix.purr() // prints Felix the cat: prrr
-felix.hiss() // prints Felix the cat: hsss
+const lynx = Object.create(leopard, {
+  purr: {
+    value: function () {
+      console.log("Felix the cat: prrr");
+    },
+  },
+});
+
+const cat = Object.create(lynx, {
+  meow: {
+    value: function () {
+      console.log("Felix the cat: meow");
+    },
+  },
+});
+
+const felix = Object.create(cat);
+
+felix.meow(); // prints Felix the cat: meow
+felix.purr(); // prints Felix the cat: prrr
+felix.hiss(); // prints Felix the cat: hsss
 
 // prototype checks, do not remove
-const felixProto = Object.getPrototypeOf(felix)
-const felixProtoProto = Object.getPrototypeOf(felixProto)
-const felixProtoProtoProto = Object.getPrototypeOf(felixProtoProto)
+const felixProto = Object.getPrototypeOf(felix);
+const felixProtoProto = Object.getPrototypeOf(felixProto);
+const felixProtoProtoProto = Object.getPrototypeOf(felixProtoProto);
 
-assert(Object.getOwnPropertyNames(felixProto).length, 1)
-assert(Object.getOwnPropertyNames(felixProtoProto).length, 1)
-assert(Object.getOwnPropertyNames(felixProtoProto).length, 1)
-assert(typeof felixProto.meow, 'function')
-assert(typeof felixProtoProto.purr, 'function')
-assert(typeof felixProtoProtoProto.hiss, 'function')
-console.log('prototype checks passed!')
+assert(Object.getOwnPropertyNames(felixProto).length === 1);
+assert(Object.getOwnPropertyNames(felixProtoProto).length === 1);
+assert(Object.getOwnPropertyNames(felixProtoProtoProto).length === 1);
+assert(typeof felixProto.meow === "function");
+assert(typeof felixProtoProto.purr === "function");
+assert(typeof felixProtoProtoProto.hiss === "function");
+
+console.log("prototype checks passed!");

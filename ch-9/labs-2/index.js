d036f6e02c697cf51d6f0869ceac6a9a782634a3
lontsimesmer <tchoupounathanael@gmail.com>
2024-04-16 23:57:42 +0100

    add another event listerner

--- ch-9/labs-2/index.js
+++ ch-9/labs-2/index.js
@@ -1,8 +1,23 @@
 "use strict";
-import { EventEmitter } from require('events');
+
+const { EventEmitter } = require("events");
 
 process.on("uncaughtException", (err) => {
-  console.log(err.message);
+  console.error("Uncaught Exception:", err);
+  // Perform any necessary cleanup or graceful shutdown here
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("Unhandled Promise Rejection:", reason);
+  // Perform any necessary cleanup or graceful shutdown here
+});
+
+// Example code that triggers an unhandled error
+
+const fs = require("fs");
+fs.readFile("nonexistent-file.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
 
 process.nextTick(console.log, "passed!");

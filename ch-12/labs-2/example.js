0d4ef660268bd9825eef3b2cc4e22830e50e40ad
lontsimesmer <tchoupounathanael@gmail.com>
2024-04-27 00:09:02 +0100

    add example for creating transform stream

--- ch-12/labs-2/example.js
+++ ch-12/labs-2/example.js
@@ -8,7 +8,8 @@ class DoubleTransform extends Transform {
 
   _transform(chunks, encoding, callback) {
     const transformedChunks = chunks.map((num) => num * 2);
-    this.push(transformedChunks);
+    const buffer = Buffer.from(JSON.stringify(transformedChunks));
+    this.push(buffer);
     callback();
   }
 }
@@ -19,10 +20,19 @@ const readable = Readable.from([[1, 2, 3, 4, 5]]);
 // Create a writable stream
 const writable = new Writable({
   write(chunk, encoding, callback) {
-    console.log("Received transformed data:", chunk);
+    console.log("Received transformed data:", chunk.toString());
     callback();
   },
 });
 
 // Create an instance of the custom transform stream
 const doubleTransform = new DoubleTransform();
+
+// Build the pipeline
+pipeline(readable, doubleTransform, writable, (err) => {
+  if (err) {
+    console.log("Pipeline failed:", err);
+  } else {
+    console.log("Pipeline completed successfully!");
+  }
+});

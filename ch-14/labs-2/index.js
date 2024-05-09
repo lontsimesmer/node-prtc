86315a409ecd6de80a05122047704f7592cd3171
lontsimesmer <tchoupounathanael@gmail.com>
2024-05-09 23:51:50 +0100

    correct os uptime and memory

--- ch-14/labs-2/index.js
+++ ch-14/labs-2/index.js
@@ -1,20 +1,10 @@
 "use strict";
 
 const os = require("os");
-const v8 = require("v8");
-
-const inSecondsUptime = os.uptime();
-const uptimeInSeconds = process.uptime();
-
-const totalMemoryInBytes = os.totalmem();
-const totalMemoryInGB = Math.floor(totalMemoryInBytes / (1024 * 1024 * 1024));
-
-const totalHeapSizeInBytes = v8.getHeapStatistics().total_heap_size;
-const totalHeapSizeInMB = Math.floor(totalHeapSizeInBytes / (1024 * 1024));
 
 setTimeout(() => {
-  console.log(process.uptime()); // TODO output uptime of process
-  console.log(os.uptime()); // TODO output uptime of OS
-  console.log(os.totalmem()); // TODO output total system memory
-  console.log(v8.getHeapStatistics().total_heap_size); // TODO output total heap memory
+  console.log(process.uptime()); // Output uptime of the process
+  console.log(os.uptime()); // Output uptime of OS
+  console.log(os.totalmem()); // Output total system memory
+  console.log(process.memoryUsage().heapTotal); // Output total heap memory
 }, 1000);

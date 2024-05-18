0ad2a758b2571d63b6cb539b90e75b2e1df52cac
lontsimesmer <tchoupounathanael@gmail.com>
2024-05-18 20:38:23 +0100

    add test.js with promise method

---
+++ ch-8/labs-2/test.js
@@ -0,0 +1,41 @@
+const { promisify } = require("util");
+const print = (err, contents) => {
+  if (err) console.error(err);
+  else console.log(contents);
+};
+const opA = (cb) => {
+  setTimeout(() => {
+    cb(null, "A");
+  }, 500);
+};
+const opB = (cb) => {
+  setTimeout(() => {
+    cb(null, "B");
+  }, 250);
+};
+const opC = (cb) => {
+  setTimeout(() => {
+    cb(null, "C");
+  }, 125);
+};
+
+// Convert the callback-based functions to promise-based functions
+const promiseOpA = promisify(opA);
+const promiseOpB = promisify(opB);
+const promiseOpC = promisify(opC);
+
+// Call the functions in the desired order using promises
+async function runSerialOperations() {
+  try {
+    const resultA = await promiseOpA();
+    print(null, `[${resultA}]`);
+    const resultB = await promiseOpB();
+    print(null, `[${resultB}]`);
+    const resultC = await promiseOpC();
+    print(null, `[${resultC}]`);
+  } catch (error) {
+    print(error);
+  }
+}
+
+runSerialOperations();

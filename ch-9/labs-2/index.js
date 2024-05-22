502c797900b79c218249fbc81f047322195e171e
lontsimesmer <tchoupounathanael@gmail.com>
2024-05-22 23:59:47 +0100

    modify the single use listener

--- ch-9/labs-2/index.js
+++ ch-9/labs-2/index.js
@@ -2,27 +2,13 @@
 
 const { EventEmitter } = require("events");
 
-process.on("uncaughtException", (err) => {
-  console.error("Uncaught Exception:", err);
-  // Perform any necessary cleanup or graceful shutdown here
-});
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.log("Unhandled Promise Rejection:", reason);
-  // Perform any necessary cleanup or graceful shutdown here
-});
-
-// Example code that triggers an unhandled error
-
-const fs = require("fs");
-fs.readFile("nonexistent-file.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
-
 process.nextTick(console.log, "passed!");
 
 const ee = new EventEmitter();
 
 ee.once("error", (err) => console.log(err.message));
+ee.on("error", (err) => {
+  // Handle the error here, for example, by logging it
+  console.error("An error occurred:", err.message);
+});
 ee.emit("error", Error("timeout"));

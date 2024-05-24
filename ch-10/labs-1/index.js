c2f200043a2f5c7c928e0533235e48ea8e1decab
lontsimesmer <tchoupounathanael@gmail.com>
2024-05-24 23:07:32 +0100

    modify synchronous error handling

--- ch-10/labs-1/index.js
+++ ch-10/labs-1/index.js
@@ -1,16 +1,12 @@
 "use strict";
 const assert = require("assert");
 
-const invalidUrl = "not a valid url";
-const result = parseUrl(invalidUrl);
-console.log(result);
-
 function parseUrl(str) {
   try {
     const parsed = new URL(str);
     return parsed;
   } catch (error) {
-    // return null;
+    return null;
   }
 }
 
@@ -19,4 +15,5 @@ assert.doesNotThrow(() => {
 });
 assert.equal(parseUrl("invalid-url"), null);
 assert.deepEqual(parseUrl("http://example.com"), new URL("http://example.com"));
+
 console.log("passed!");

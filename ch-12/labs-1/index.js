44bc490f42a293047ff5b833ed5bc65b0bc977a2
lontsimesmer <tchoupounathanael@gmail.com>
2024-06-03 23:57:51 +0100

    modify the piping exercice

--- ch-12/labs-1/index.js
+++ ch-12/labs-1/index.js
@@ -31,22 +31,5 @@ const createWritable = () => {
 const readable = Readable.from(["a", "b", "c"]);
 const writable = createWritable();
 
-// Connect the readable stream to the writable stream using pipe
+// Send all data from readable to writable using the pipe() method
 readable.pipe(writable);
-
-// Create the writable stream
-const writable1 = new Writable({
-  write(chunk, encoding, callback) {
-    // Handle the writable stream data here
-    console.log("Received data:", chunk.toString());
-    callback();
-  },
-});
-
-// Create the readable streams
-const readable1 = Readable.from(["Hello"]);
-const readable2 = Readable.from(["World"]);
-
-// Connect the readable streams to the writable stream using pipe
-readable1.pipe(writable1);
-readable2.pipe(writable1);

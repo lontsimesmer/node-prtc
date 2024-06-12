8c5f02f462ea6e6f3d0d44a2a406a16c7d08b996
lontsimesmer <tchoupounathanael@gmail.com>
2024-06-12 22:29:16 +0100

    add many regex extensions

--- ch-14/labs-1/example.js
+++ ch-14/labs-1/example.js
@@ -1,37 +1,36 @@
-// // Operating system names validation
+// Operating system names validation
 
-// const osNames = [
-//   "Windows",
-//   "Linux",
-//   "MacOS",
-//   "sunOS",
-//   "FreeBSD",
-//   "AIX",
-//   "GNU/Linux",
-// ];
-// const regex = /^(d|w|l|aix|.+bsd|sunos|macos|gnu)/i;
+const osNames = [
+  "Windows",
+  "Linux",
+  "MacOS",
+  "sunOS",
+  "FreeBSD",
+  "AIX",
+  "GNU/Linux",
+];
+const regex = /^(e|d|w|l|aix|.+bsd|sunos|macos|gnu|js)/i;
 
-// for (const osName of osNames) {
-//   if (regex.test(osName)) {
-//     console.log(`${osName} matches the regex pattern.`);
-//   } else {
-//     console.log(`${osName} does not matches the regex pattern.`);
-//   }
-// }
+for (const osName of osNames) {
+  if (regex.test(osName)) {
+    console.log(`${osName} matches the regex pattern.`);
+  } else {
+    console.log(`${osName} does not matches the regex pattern.`);
+  }
+}
 
 // File names filtering in a directory
 
 const fs = require("fs");
 const path = require("path");
-const regex = /^(e|d|w|l|aix|.+bsd|sunos|macos|gnu|js)/i;
 
-const directory = process.cwd();
+// const directory = process.cwd()
+// console.log(directory);
 
-console.log(directory);
+const directory = "/Users/HP/OneDrive/Documents/node-practice/ch-14/labs-1";
 
 if (fs.existsSync(directory) && fs.statSync(directory).isDirectory()) {
   const files = fs.readdirSync(directory);
-
   console.log(files);
 
   for (const filename of files) {

c4bc49d3a748ac91336fa200b4f4972bc0e38bfc
lontsimesmer <tchoupounathanael@gmail.com>
2024-06-19 23:59:10 +0100

    modify the index file of ch-14

--- ch-14/labs-2/index.js
+++ ch-14/labs-2/index.js
@@ -1,10 +1,20 @@
-'use strict'
+'use strict';
 
-const os = require('os')
+const os = require('os');
+
+function getProcessUptime() {
+  // Get the uptime of the current process in seconds
+  return process.uptime();
+}
+
+function getOsUptime() {
+  // Get the operating system uptime in seconds
+  return os.uptime();
+}
 
 setTimeout(() => {
-  console.log(process.uptime()) // Output uptime of the process
-  console.log(os.uptime()) // Output uptime of OS
-  console.log(os.totalmem()) // Output total system memory
-  console.log(process.memoryUsage().heapTotal) // Output total heap memory
-}, 1000)
+  console.log(getProcessUptime()); // Output uptime of the process
+  console.log(getOsUptime()); // Output uptime of OS
+  console.log(os.totalmem()); // Output total system memory
+  console.log(process.memoryUsage().heapTotal); // Output total heap memory
+}, 1000);

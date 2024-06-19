781d55f443cbb33e0bc67c4dd3e892513a29ee72
lontsimesmer <tchoupounathanael@gmail.com>
2024-06-20 00:18:56 +0100

    modify the index file of ch-14/labs-2

--- ch-14/labs-1/test.js
+++ ch-14/labs-1/test.js
@@ -1,15 +1,16 @@
-'use strict'
-const { spawnSync } = require('child_process')
-const assert = require('assert')
+'use strict';
 
-const { status, stdout } = spawnSync(process.argv[0], [__dirname])
+const { spawnSync } = require('child_process');
+const assert = require('assert');
 
-assert.notStrictEqual(status, 0, 'must exit with a non-zero code')
-assert(stdout !== null, 'stdout should not be null')
+const { status, stdout } = spawnSync(process.argv[0], [__dirname]);
+
+assert.notStrictEqual(status, 0, 'must exit with a non-zero code');
+assert(stdout !== null, 'stdout should not be null');
 assert.match(
-    stdout.toString(),
-    /win32/i, // Update the regular expression to match with win32
-    'must output OS identifier'
-)
+  stdout.toString(),
+  /win32/i, // Update the regular expression to match with win32
+  'must output OS identifier'
+);
 
-console.log('passed!')
+console.log('passed!');
--- ch-14/labs-2/index.js
+++ ch-14/labs-2/index.js
@@ -12,9 +12,19 @@ function getOsUptime() {
   return os.uptime();
 }
 
+function getOsTotalmem() {
+  // Get the total system memory in bytes
+  return os.totalmem();
+}
+
+function getOsTotalHeapMemory() {
+  // Get the total heap memory used by the process in bytes
+  return process.memoryUsage().heapTotal;
+}
+
 setTimeout(() => {
   console.log(getProcessUptime()); // Output uptime of the process
   console.log(getOsUptime()); // Output uptime of OS
-  console.log(os.totalmem()); // Output total system memory
-  console.log(process.memoryUsage().heapTotal); // Output total heap memory
+  console.log(getOsTotalmem()); // Output total system memory
+  console.log(getOsTotalHeapMemory()); // Output total heap memory
 }, 1000);

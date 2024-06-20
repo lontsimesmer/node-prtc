fdc023f4375ef1627459349b051601e0af595d01
lontsimesmer <tchoupounathanael@gmail.com>
2024-06-20 22:45:10 +0100

    modify the .prettierrc file

--- ch-14/labs-1/example.js
+++ ch-14/labs-1/example.js
@@ -8,38 +8,38 @@ const osNames = [
   'FreeBSD',
   'AIX',
   'GNU/Linux',
-]
-const regex = /^(e|d|w|l|aix|.+bsd|sunos|macos|gnu|js)/i
+];
+const regex = /^(e|d|w|l|aix|.+bsd|sunos|macos|gnu|js)/i;
 
 for (const osName of osNames) {
   if (regex.test(osName)) {
-    console.log(`${osName} matches the regex pattern.`)
+    console.log(`${osName} matches the regex pattern.`);
   } else {
-    console.log(`${osName} does not matches the regex pattern.`)
+    console.log(`${osName} does not matches the regex pattern.`);
   }
 }
 
 // File names filtering in a directory
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 // const directory = process.cwd()
 // console.log(directory);
 
-const directory = '/Users/HP/OneDrive/Documents/node-practice/ch-14/labs-1'
+const directory = '/Users/HP/OneDrive/Documents/node-practice/ch-14/labs-1';
 
 if (fs.existsSync(directory) && fs.statSync(directory).isDirectory()) {
-  const files = fs.readdirSync(directory)
-  console.log(files)
+  const files = fs.readdirSync(directory);
+  console.log(files);
 
   for (const filename of files) {
     if (regex.test(filename)) {
-      console.log(`${filename} matches the regex pattern.`)
+      console.log(`${filename} matches the regex pattern.`);
     }
   }
 } else {
-  console.log(`Error: "${directory}" is not a valid directory.`)
+  console.log(`Error: "${directory}" is not a valid directory.`);
 }
 
 // Information extraction from log files
@@ -52,15 +52,15 @@ const logContent = `
 2024-06-13 11:51:03 - OS: sunOs
 2024-06-13 11:51:04 - OS: FreeBSD
 2024-06-13 11:51:05 - OS: AIX
-`
+`;
 
-const lines = logContent.trim().split('\n')
-const regexp = /OS:\s*(\w+)/i
+const lines = logContent.trim().split('\n');
+const regexp = /OS:\s*(\w+)/i;
 
 for (const line of lines) {
-  const parts = line.match(regexp)
+  const parts = line.match(regexp);
   if (parts) {
-    console.log(`Operating system detected: ${parts[1]}`)
+    console.log(`Operating system detected: ${parts[1]}`);
   }
 }
 // const matches = logContent.match(regexp);

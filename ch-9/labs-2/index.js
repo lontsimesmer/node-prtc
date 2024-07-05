5e921ea7f75be3807f86b770ca970031d3861569
lontsimesmer <tchoupounathanael@gmail.com>
2024-07-05 23:59:49 +0100

    update single use listener

--- ch-9/labs-2/index.js
+++ ch-9/labs-2/index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
-const { EventEmitter } = require("events");
+const { EventEmitter } = require('events');
 
-process.nextTick(console.log, "passed!");
+process.nextTick(console.log, 'passed!');
 
 const ee = new EventEmitter();
 
-ee.once("error", (err) => console.log(err.message));
-ee.on("error", (err) => {
+ee.once('error', (err) => console.log(err.message));
+ee.on('error', (err) => {
   // Handle the error here, for example, by logging it
-  console.error("An error occurred:", err.message);
+  console.error('An error occurred:', err.message);
 });
-ee.emit("error", Error("timeout"));
+ee.emit('error', Error('timeout'));

98cf462fab8476e583ef6ceffba1f6a3b0d3f425
lontsimesmer <tchoupounathanael@gmail.com>
2024-07-17 22:47:34 +0100

    add another test method

--- ch-12/labs-1/test.js
+++ ch-12/labs-1/test.js
@@ -1,20 +1,28 @@
-"use strict";
-const { Readable, Writable } = require("stream");
-// const assert = require("assert");
+'use strict';
+const { Readable, Writable } = require('stream');
+const assert = require('assert');
 
 // Create the writable stream
-const writable1 = new Writable({
-  write(chunk, encoding, callback) {
+const writable = new Writable({
+  write(chunk, _encoding, callback) {
     // Handle the writable stream data here
-    console.log("Received data:", chunk.toString());
+    console.log('Received data:', chunk.toString());
     callback();
   },
 });
 
 // Create the readable streams
-const readable1 = Readable.from(["Hello"]);
-const readable2 = Readable.from(["World"]);
+const readable1 = Readable.from(['Hello']);
+const readable2 = Readable.from(['World']);
 
 // Connect the readable streams to the writable stream using pipe
-readable1.pipe(writable1);
-readable2.pipe(writable1);
+readable1.pipe(writable);
+readable2.pipe(writable);
+
+// Create a combined readable stream
+const combinedReadable = Readable.from(['Hello World']);
+
+// Read the data from the combined readable stream and log "Hello World"
+combinedReadable.on('data', (chunk) => {
+  console.log(chunk.toString());
+});

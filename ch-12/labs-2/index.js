17312e24226f69772a3494187c5361061f2ace3b
lontsimesmer <tchoupounathanael@gmail.com>
2024-07-18 21:57:14 +0100

    arrange the read and write directory

--- ch-12/labs-2/index.js
+++ ch-12/labs-2/index.js
@@ -1,6 +1,6 @@
-"use strict";
-const { Readable, Writable, Transform, pipeline } = require("stream");
-const assert = require("assert");
+'use strict';
+const { Readable, Writable, Transform, pipeline } = require('stream');
+const assert = require('assert');
 
 const createWritable = () => {
   const sink = [];
@@ -14,34 +14,34 @@ const createWritable = () => {
   return writable;
 };
 
-const readable = Readable.from(["a", "b", "c"]);
+const readable = Readable.from(['a', 'b', 'c']);
 const writable = createWritable();
 
 // Custom transform stream to uppercase incoming chunks
 
-// class UppercaseTransform extends Transform {
-//   _transform(chunk, encoding, callback) {
-//     const uppercasedChunk = chunk.toString().toUpperCase();
-//     this.push(uppercasedChunk);
-//     callback();
-//   }
-// }
+class UppercaseTransform extends Transform {
+  _transform(chunk, encoding, callback) {
+    const uppercasedChunk = chunk.toString().toUpperCase();
+    this.push(uppercasedChunk);
+    callback();
+  }
+}
 
-// const transform = new UppercaseTransform();
+const transform = new UppercaseTransform();
 
 // Replace the PassThrough stream with a Transform stream that uppercases the incoming chunks
 
-const transform = new Transform({
-  transform(chunk, encoding, callback) {
-    callback(null, chunk.toString().toUpperCase());
-    // const uppercasedChunk = chunk.toString().toUpperCase();
-    // this.push(uppercasedChunk);
-    // callback();
-  },
-});
+// const transform = new Transform({
+//   transform(chunk, encoding, callback) {
+//     callback(null, chunk.toString().toUpperCase());
+//     // const uppercasedChunk = chunk.toString().toUpperCase();
+//     // this.push(uppercasedChunk);
+//     // callback();
+//   },
+// });
 
 pipeline(readable, transform, writable, (err) => {
   assert.ifError(err);
-  assert.deepStrictEqual(writable.sink, ["A", "B", "C"]);
-  console.log("passed!");
+  assert.deepStrictEqual(writable.sink, ['A', 'B', 'C']);
+  console.log('passed!');
 });
--- ch-13/labs-1/index.js
+++ ch-13/labs-1/index.js
@@ -1,9 +1,9 @@
-"use strict";
-const assert = require("assert");
-const { join } = require("path");
-const fs = require("fs");
+'use strict';
+const assert = require('assert');
+const { join } = require('path');
+const fs = require('fs');
 
-const project = join(__dirname, "project");
+const project = join(__dirname, 'project');
 try {
   fs.rmSync(project, { recursive: true });
 } catch (err) {}
@@ -13,41 +13,23 @@ const files = Array.from(Array(5), () => {
 });
 
 fs.mkdirSync(project);
-for (const f of files) fs.closeSync(fs.openSync(f, "w"));
+for (const f of files) fs.closeSync(fs.openSync(f, 'w'));
 
-const out = join(__dirname, "out.txt");
+const out = join(__dirname, 'out.txt');
 
 function exercise() {
   // Read the file in the projects folder
   const fileNames = files.map((file) => path.basename(file));
 
   // Write the file names to the out.txt folder
-  fs.writeFileSync(out, fileNames.join(","));
+  fs.writeFileSync(out, fileNames.join(','));
 }
-
-// function exercise() {
-//   const fileNames = [];
-//   const projectFiles = fs.readdirSync(project);
-
-//   for (const file of projectFiles) {
-//     const fileName = join(project, file);
-//     const stats = fs.statSync(fileName);
-
-//     if (stats.isFile()) {
-//       fileNames.push(file);
-//     }
-//   }
-
-//   const content = fileNames.join(",");
-
-//   fs.writeFileSync(out, content);
-// }
 
 exercise();
 
-const expected = files.map((f) => require("path").basename(f)).sort();
-const actual = fs.readFileSync(out).toString().trim().split(",").sort();
+const expected = files.map((f) => require('path').basename(f)).sort();
+const actual = fs.readFileSync(out).toString().trim().split(',').sort();
 
 assert.deepStrictEqual(actual, expected);
 
-console.log("passed!");
+console.log('passed!');
